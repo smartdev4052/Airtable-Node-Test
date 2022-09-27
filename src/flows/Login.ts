4348272b579571df8221c453e17b84a12a9af3a5
thamilton2567 <thamilton2567@gmail.com>
2022-09-27 15:32:11 -0400

    Updated

---
+++ src/flows/Login.ts
@@ -0,0 +1,4 @@
+export default function* Login(student: string) {
+  // store student to state
+  yield { set: { student } };
+}
---
+++ src/types.d.ts
@@ -0,0 +1,9 @@
+export type StudentFields = {
+  Name: string;
+  Classes: ReadonlyArray<string>;
+};
+
+export type ClassFields = {
+  Name: string;
+  Students: ReadonlyArray<string>;
+};
